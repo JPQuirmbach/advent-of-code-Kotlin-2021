89f24f7a874f45529e0d210ddb435279c37ec456
Jan-Philip Quirmbach <jan-philip.quirmbach@zuehlke.com>
2021-12-01 16:09:56 +0100

    Day1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,10 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        for ( i  in 1 until input.size)
+            if(input[i].toInt() > input[i-1].toInt())
+                result++
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +13,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
