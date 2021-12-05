47a302ffbf619d90a955b5d98b33dd189893a484
Jan-Philip Quirmbach <jan-philip.quirmbach@zuehlke.com>
2021-12-05 12:14:18 +0100

    Day5 part 2

--- src/main/kotlin/solutions/Day05.kt
+++ src/main/kotlin/solutions/Day05.kt
@@ -3,6 +3,7 @@ package solutions
 import readInput
 import java.lang.Integer.max
 import java.lang.Integer.min
+import kotlin.math.abs
 
 fun main() {
 
@@ -41,18 +42,42 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-
-        return input.size
+        val (lines, max) = parseInput(input)
+        val grid = Array(max.second + 1) { Array(max.first + 1) { 0 } }
+        lines.filter { line ->
+            line.p1.x == line.p2.x || line.p1.y == line.p2.y ||
+                    abs(line.p1.x - line.p2.x) == abs(line.p1.y - line.p2.y)
+        }
+            .forEach { line ->
+                val (p1, p2) = line
+                if (p1.x == p2.x) {
+                    for (y in min(p1.y, p2.y)..max(p1.y, p2.y)) {
+                        grid[y][p1.x]++
+                    }
+                } else if (p1.y == p2.y) {
+                    for (x in min(p1.x, p2.x)..max(p1.x, p2.x)) {
+                        grid[p1.y][x]++
+                    }
+                } else {
+                    val d = abs(p1.x - p2.x)
+                    val dx = (p2.x - p1.x) / d
+                    val dy = (p2.y - p1.y) / d
+                    for (i in 0..d) {
+                        grid[p1.y + dy * i][p1.x + dx * i]++
+                    }
+                }
+            }
+        return grid.sumOf { it.count { it > 1 } }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 5)
-//    check(part2(testInput) == 1924)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day05")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 data class Point(val x: Int, val y: Int)
